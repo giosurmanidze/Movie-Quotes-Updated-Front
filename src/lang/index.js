0f99173ece52e7605e399fe3b28d36c6b8d9e48d
giosurmanidze <giorgisurmanidze917@gmail.com>
2023-05-22 22:07:31 +0400

    add locale switcher

---
+++ src/lang/index.js
@@ -0,0 +1,19 @@
+import { createI18n } from 'vue-i18n'
+import * as landingEn from './en/landing.json'
+import * as landingKa from './ka/landing.json'
+
+const i18n = createI18n({
+  locale: localStorage.getItem('lang') ? localStorage.getItem('lang') : 'en',
+  fallbackLocale: 'en',
+  legacy: false,
+  messages: {
+    en: {
+      ...landingEn
+    },
+    ka: {
+      ...landingKa
+    }
+  }
+})
+
+export default i18n
