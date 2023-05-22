a1972760413425647b05740fb8d0c9264082ebbe
giosurmanidze <giorgisurmanidze917@gmail.com>
2023-05-22 20:51:15 +0400

    style/landing screen

---
+++ src/components/Button.vue
@@ -0,0 +1,18 @@
+<template>
+  <button class="text-white py-2 px-3 rounded-[4px] lg:text-xl" :class="styles">{{ text }}</button>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  text: {
+    type: String,
+    required: true
+  },
+  styles: {
+    type: String,
+    required: false
+  }
+})
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="w-full flex justify-between items-center">
+    <span class="text-[#DDCCAA]">MOVIE QUOTES</span>
+    <div class="flex items-center gap-6 text-[#fff]">
+      <span class="xs:hidden sm:flex items-center gap-2 lg:text-xl"
+        >Eng <img src="../assets/caret-right-fill.svg" class="mt-1"
+      /></span>
+      <Button text="Sign up" styles="bg-[#E31221]" />
+      <Button text="Log in" styles="border-2 border-[#fff]" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Button from './Button.vue'
+</script>
