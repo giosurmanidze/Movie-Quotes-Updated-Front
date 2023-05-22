17016c738bbaf444dbe451767d07e5fa892743c5
giosurmanidze <giorgi.surmanidze@redberry.ge>
2023-05-22 20:51:15 +0400

    style/landing screen

---
+++ src/pages/Landing.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="lg:block">
+    <div class="h-[80vh] bg-banner_gradient pt-8 px-8 lg:px-16">
+      <Header />
+      <div class="h-[60vh] w-full flex justify-center items-center">
+        <div class="flex flex-col items-center space-y-6 w-[400px] lg:w-[800px]">
+          <h1 class="text-[#DDCCAA] text-2xl lg:text-6xl text-center">
+            Find any quote in millions of movie lines
+          </h1>
+          <Button text="Get Started" styles="bg-[#E31221]" />
+        </div>
+      </div>
+    </div>
+    <LandingImageCard />
+  </div>
+</template>
+
+<script setup>
+import Header from '../components/Header.vue'
+import Button from '../components/Button.vue'
+import LandingImageCard from '../components/LandingImageCard.vue'
+</script>
---
+++ src/stores/quotes.js
@@ -0,0 +1,21 @@
+export const quotes = [
+  {
+    img: 'https://i.postimg.cc/4dpWbhzk/landing-page.png',
+    quote: '“You have to leave somethig behind to go forward”',
+    movie: 'Interstellar',
+    year: '2014'
+  },
+  {
+    img: 'https://i.postimg.cc/4dpWbhzk/landing-page.png',
+    quote:
+      'I think we’re just gonna have to be secretly in love with each other and leave it at that',
+    movie: 'Movie Name',
+    year: 'Year'
+  },
+  {
+    img: 'https://i.postimg.cc/4dpWbhzk/landing-page.png',
+    quote: '“You have to leave somethig behind to go forward”',
+    movie: 'Another Movie',
+    year: 'Another Year'
+  }
+]
