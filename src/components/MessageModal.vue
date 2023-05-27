a3e4249d695a5e9fb3f85106c769df24e78ad306
giosurmanidze <giorgisurmanidze917@gmail.com>
2023-05-27 21:40:03 +0400

    fix/modal styles

--- src/components/MessageModal.vue
+++ src/components/MessageModal.vue
@@ -1,20 +1,24 @@
 <template>
   <div
-    class="modal-content py-8 max-w-[500px] sm:px-16 xs:px-10 rounded shadow-lg flex flex-col gap-2 xs:w-[95%] max-h-90vh overflow-auto bg-[#222030]"
-    @click.stop
+    class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-60 backdrop-filter backdrop-blur-sm"
   >
-    <div class="flex flex-col justify-center items-center gap-10 text-[#FFFFFF] h-[40vh]">
-      <img src="../assets/email-sent.svg" width="90" height="90" />
-      <h1 class="text-4xl">{{ headerText }}</h1>
-      <p class="text-center w-full text-md">
-        {{ msg }}
-      </p>
-      <Button
-        :text="btnText"
-        :classes="classes"
-        :route="route"
-        @click="dialogStore.dialog = dialogName"
-      />
+    <div
+      class="modal-content py-8 max-w-[500px] sm:px-16 xs:px-10 rounded shadow-lg flex flex-col gap-2 xs:w-[95%] max-h-90vh overflow-auto bg-[#222030]"
+      @click.stop
+    >
+      <div class="flex flex-col justify-center items-center gap-10 text-[#FFFFFF] h-[40vh]">
+        <img src="../assets/email-sent.svg" width="90" height="90" />
+        <h1 class="text-4xl">{{ headerText }}</h1>
+        <p class="text-center w-full text-md">
+          {{ msg }}
+        </p>
+        <Button
+          :text="btnText"
+          :classes="classes"
+          :route="route"
+          @click="dialogStore.dialog = dialogName"
+        />
+      </div>
     </div>
   </div>
 </template>
--- src/pages/SignupForm.vue
+++ src/pages/SignupForm.vue
@@ -1,61 +1,65 @@
 <template>
   <div
-    class="modal-content py-8 sm:px-16 xs:px-10 max-w-[600px] rounded shadow-lg flex flex-col gap-4 w-[95%] max-h-90vh overflow-auto bg-[#222030]"
-    @click.stop
+    class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-60 backdrop-filter backdrop-blur-sm"
   >
-    <div class="flex flex-col justify-center items-center text-[#FFFFFF] gap-1">
-      <h1 class="text-3xl">{{ $t('sign_up_form_title') }}</h1>
-      <span class="text-md text-[#6C757D]">{{ $t('sign_up_form_header_text') }}</span>
-    </div>
-    <Form @submit="submit" v-slot="{ errors }">
-      <div class="flex flex-col justify-between h-[55vh]">
-        <text-field
-          label="username"
-          type="text"
-          name="name"
-          rules="required|min:3|max:15|lower_alpha_num"
-          :placeholder="$t('min_max_and_lower_case', { min: 3, max: 15 })"
-          :hasError="errors.name"
-        />
-        <text-field
-          label="email"
-          type="text"
-          name="email"
-          rules="required|email"
-          :placeholder="$t('enter_email')"
-          :hasError="errors.email"
-        />
-        <text-field
-          label="password"
-          type="password"
-          name="password"
-          rules="required|min:8|max:15|lower_alpha_num"
-          :placeholder="$t('min_max_and_lower_case', { min: 8, max: 15 })"
-          :hasError="errors.password"
-        />
-        <text-field
-          label="conf_password"
-          type="password"
-          name="password_confirmation"
-          rules="required|confirmed:@password"
-          :placeholder="$t('conf_password')"
-          :hasError="errors.password_confirmation"
-        />
-        <submit-button text="sign_up_btn_in_form" classes="bg-[#E31221]" />
-        <submit-button
-          text="sign_up_with_google"
-          classes="flex justify-center items-center gap-3 border-2 pb-1 border-[#ffffff]"
-          :show-icon="true"
-        />
-      </div>
-      <Loading v-if="loading" />
-    </Form>
-    <span class="text-center text-[#6C757D]"
-      >{{ $t('sign_up_footer_text')
-      }}<span @click="showModal('login')" class="text-[#0D6EFD] underline ml-1">
-        {{ $t('log_in_btn') }}
-      </span></span
+    <div
+      class="modal-content py-8 sm:px-16 xs:px-10 max-w-[600px] rounded shadow-lg flex flex-col gap-4 w-[95%] max-h-90vh overflow-auto bg-[#222030]"
+      @click.stop
     >
+      <div class="flex flex-col justify-center items-center text-[#FFFFFF] gap-1">
+        <h1 class="text-3xl">{{ $t('sign_up_form_title') }}</h1>
+        <span class="text-md text-[#6C757D]">{{ $t('sign_up_form_header_text') }}</span>
+      </div>
+      <Form @submit="submit" v-slot="{ errors }">
+        <div class="flex flex-col justify-between h-[55vh]">
+          <text-field
+            label="username"
+            type="text"
+            name="name"
+            rules="required|min:3|max:15|lower_alpha_num"
+            :placeholder="$t('min_max_and_lower_case', { min: 3, max: 15 })"
+            :hasError="errors.name"
+          />
+          <text-field
+            label="email"
+            type="text"
+            name="email"
+            rules="required|email"
+            :placeholder="$t('enter_email')"
+            :hasError="errors.email"
+          />
+          <text-field
+            label="password"
+            type="password"
+            name="password"
+            rules="required|min:8|max:15|lower_alpha_num"
+            :placeholder="$t('min_max_and_lower_case', { min: 8, max: 15 })"
+            :hasError="errors.password"
+          />
+          <text-field
+            label="conf_password"
+            type="password"
+            name="password_confirmation"
+            rules="required|confirmed:@password"
+            :placeholder="$t('conf_password')"
+            :hasError="errors.password_confirmation"
+          />
+          <submit-button text="sign_up_btn_in_form" classes="bg-[#E31221]" />
+          <submit-button
+            text="sign_up_with_google"
+            classes="flex justify-center items-center gap-3 border-2 pb-1 border-[#ffffff]"
+            :show-icon="true"
+          />
+        </div>
+        <Loading v-if="loading" />
+      </Form>
+      <span class="text-center text-[#6C757D]"
+        >{{ $t('sign_up_footer_text')
+        }}<span @click="showModal('login')" class="text-[#0D6EFD] underline ml-1">
+          {{ $t('log_in_btn') }}
+        </span></span
+      >
+    </div>
   </div>
 </template>
 
