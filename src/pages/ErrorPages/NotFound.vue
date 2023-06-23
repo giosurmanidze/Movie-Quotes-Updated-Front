f728f168b39281499d8ff2616b25a64c4bfc1daa
giosurmanidze <giorgisurmanidze917@gmail.com>
2023-06-24 00:07:41 +0400

    feat/add error pages and protecting check

---
+++ src/pages/ErrorPages/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="bg-[#181623] grid h-screen place-items-center text-white pb-20">
+    <section class="flex flex-col items-center">
+      <not-found-page-icon />
+      <h1 class="md:text-4xl text-2xl font-bold mt-10">{{ $t("whoops_text") }}</h1>
+      <p class="md:text-lg text-base py-4 text-center px-4">
+        {{ $t("error_page_text") }}
+      </p>
+      <Button type="button" text="return_home" classes="bg-btn_color" />
+    </section>
+  </div>
+</template>
+
+<script setup>
+import NotFoundPageIcon from "@/assets/icons/NotFoundPageIcon.vue";
+import Button from "@/components/Button.vue";
+</script>
