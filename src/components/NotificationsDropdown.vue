5b30dcfa3176eb40505da9ce92e004d7b5a517de
giosurmanidze <giorgi.surmanidze@redberry.ge>
2023-07-03 12:03:22 +0400

    fix/return rem and remove unread request

--- src/components/NotificationsDropdown.vue
+++ src/components/NotificationsDropdown.vue
@@ -4,15 +4,15 @@
       <button @click="toggleDropdown" class="mr-8 inline-flex relative">
         <NotificationIcon />
         <div
-          v-if="unreadNotifications"
+          v-if="unreadNotificationCount"
           class="inline-flex absolute -top-2 -right-2 justify-center items-center w-5 h-5 text-xs font-bold text-white bg-red-500 rounded-full"
         >
-          {{ unreadNotifications }}
+          {{ unreadNotificationCount }}
         </div>
       </button>
       <div
         v-if="dropdownState"
-        class="overflow-y-auto max-h-[600px] origin-top-right px-10 py-3 md:p-6 absolute mt-2 right-0 md:right-5 bg-black w-screen md:w-[620px] rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+        class="overflow-y-auto max-h-[37.5rem] origin-top-right px-10 py-3 md:p-6 absolute mt-2 right-0 md:right-5 bg-black w-screen md:w-[38.75rem] rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
       >
         <div class="flex justify-between mt-3 mb-5">
           <p class="md:text-xl text-sm">{{ $t("notifications") }}</p>
@@ -30,7 +30,7 @@
 </template>
 
 <script setup>
-import { ref, watch, onMounted } from "vue";
+import { ref, watch, onMounted, computed } from "vue";
 import NotificationIcon from "@/assets/icons/NotificationIcon.vue";
 import NotificationBoxContent from "@/components/NotificationBoxContent.vue";
 import { useQuotesStore } from "@/stores/useQuotesStore";
@@ -40,7 +40,7 @@ import axios from "@/config/axios/index";
 
 const dropdownState = ref(false);
 const notifications = ref([]);
-const unreadNotifications = ref(0);
+
 const { getQuotesRefresh } = useQuotesStore();
 const { user } = storeToRefs(useUserStore());
 
@@ -70,17 +70,16 @@ onMounted(() => {
   axios.get("api/get-notifications").then((response) => {
     notifications.value = response.data;
   });
-  axios.get("api/get-unread-notifications").then((response) => {
-    unreadNotifications.value = response.data;
-  });
+});
+
+const unreadNotificationCount = computed(() => {
+  return notifications.value.filter((notification) => !notification.read).length;
 });
 
 function markAllAsReadHandler() {
-  unreadNotifications.value = 0;
+  unreadNotificationCount.value = 0;
   axios.post("api/mark-read").then((response) => {
     notifications.value = response.data;
   });
 }
-
-console.log(notifications);
 </script>
