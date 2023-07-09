4266069d25d968e8a0b4b62ac85d82e60cb000fa
giosurmanidze <giorgisurmanidze917@gmail.com>
2023-07-09 23:08:00 +0400

    fix/maintain searched result after reload page

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,13 +1,24 @@
 <template>
   <Field :name="name" :rules="rules" v-slot="{ field }" class="flex">
     <div class="flex">
-      <input v-bind="field" :placeholder="placeholder" :type="type" :class="classes" />
+      <input
+        v-bind="field"
+        :placeholder="placeholder"
+        :type="type"
+        :class="classes"
+        :value="searchQuery"
+        @input="updateSearchQuery($event.target.value)"
+      />
     </div>
   </Field>
 </template>
 
 <script setup>
 import { Field } from "vee-validate";
+import { useRouter, useRoute } from "vue-router";
+import { ref, watch } from "vue";
+import { usePostStore } from "@/stores/posts";
+import { useMoviesStore } from "@/stores/useMoviesStore";
 
 defineProps({
   name: { type: String, required: true },
@@ -18,5 +29,34 @@ defineProps({
   type: { type: String, required: false },
   placeholder: { type: String, required: false },
   classes: { type: String, required: false },
+});
+
+const router = useRouter();
+const route = useRoute();
+const searchStoreQuote = usePostStore();
+const searchStoreMovie = useMoviesStore();
+
+const searchQuery = ref("");
+
+if (route.query.search) {
+  searchQuery.value = route.query.search;
+  searchStoreQuote.searchValue = searchQuery.value;
+  searchStoreMovie.searchValue = searchQuery.value;
+}
+
+const updateSearchQuery = (value) => {
+  searchQuery.value = value;
+  updateRouteQuery();
+};
+
+const updateRouteQuery = () => {
+  const query = { ...route.query };
+  query.search = searchQuery.value;
+
+  router.push({ query });
+};
+
+watch(searchQuery, () => {
+  updateRouteQuery();
 });
 </script>
