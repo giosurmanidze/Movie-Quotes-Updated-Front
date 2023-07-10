a966f146b2e06bae0530fd716f0eba890b92799a
giosurmanidze <giorgi.surmanidze@redberry.ge>
2023-07-11 01:11:11 +0400

    fix/profile style

--- src/components/NoGoogleProfile.vue
+++ src/components/NoGoogleProfile.vue
@@ -53,14 +53,7 @@
               :currentUser="user.email"
               :disabled="disableInputForEmail"
             />
-            <ProfileInput
-              v-if="disableInputForEmail"
-              class="lg:w-1/2 w-full"
-              name="newEmail"
-              label="new_email"
-              rules="required"
-              :disabled="disableInputForEmail"
-            />
+
             <p
               v-if="disableInputForEmail"
               @click="inputToggleHandlerFromEmail()"
