033e79d4e52d965270f65e15f97c795b56d45423
giosurmanidze <giorgisurmanidze917@gmail.com>
2023-07-10 17:15:31 +0400

    fix/start page text size

--- src/components/LandingImageCard.vue
+++ src/components/LandingImageCard.vue
@@ -2,18 +2,18 @@
   <div>
     <div v-for="(item, index) in quotes" :key="index">
       <div
-        class="xs:h-[50vh] paralax-scroll fit md:h-screen bg-size pt-[30%] pl-[10%] lg:pt-[12.5rem] lg:pl-44"
+        class="xs:h-[50vh] paralax-scroll fit md:h-screen pt-[30%] pl-[10%] lg:pt-[12.5rem] lg:pl-44"
         :style="{ backgroundImage: `url(${item.img})` }"
       >
-        <div class="max-w-[12.5rem] md:max-w-[31.25] lg:max-w-[56.25] flex space-x-1">
+        <div class="md:max-w-[31.25] lg:max-w-[56.25] flex space-x-1">
           <div class="flex items-center h-4 lg:h-12">
             <div class="w-3 lg:w-14 xs:h-[1px] md:h-1 bg-white"></div>
           </div>
           <div>
-            <blockquote class="text-white text-sm lg:text-5xl">
+            <blockquote class="text-white xs:text-base lg:text-5xl">
               {{ $t(item.quote) }}
             </blockquote>
-            <address class="text-white text-xs lg:text-3xl mt-4">
+            <address class="text-white xs:text-sm lg:text-3xl mt-4">
               {{ $t(item.movie) }}, {{ item.year }}
             </address>
           </div>
@@ -25,15 +25,6 @@
 
 <script setup>
 import { quotes } from "@/stores/quotes";
-import { ref } from "vue";
-let scrolled = ref(false);
-window.addEventListener("scroll", () => {
-  if (window.scrollY >= window.innerHeight * 0.8) {
-    scrolled.value = true;
-  } else {
-    scrolled.value = false;
-  }
-});
 </script>
 <style scoped>
 .fit {
