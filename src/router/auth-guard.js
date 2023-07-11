14d0731778c39063fcfafe204d82362eae6073a3
giosurmanidze <giorgisurmanidze917@gmail.com>
2023-07-11 21:38:45 +0400

    fix/remove unnessesary file

--- src/router/auth-guard.js
+++ src/router/auth-guard.js
@@ -1,10 +1,16 @@
-import { useAuthStore } from "@/stores/useAuthStore";
+import { useAuthStore } from '@/stores/useAuthStore'
 
-const isAuthenticated = () => {
-  const authStore = useAuthStore();
+export const isAuthenticated = () => {
+  const authStore = useAuthStore()
   if (!authStore.authenticated) {
-    return "/forbidden";
+    return 'forbidden'
   }
-};
+}
 
-export default isAuthenticated;
+export const isNotAuthenticated = () => {
+  const authStore = useAuthStore()
+  if (authStore.authenticated) {
+    authStore.isNotAuthenticated = false
+    return 'forbidden'
+  }
+}
