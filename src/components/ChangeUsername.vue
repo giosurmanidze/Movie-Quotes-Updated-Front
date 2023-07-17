56abddf5519c032e10a9bdeed7da646002e2de24
giosurmanidze <giorgisurmanidze917@gmail.com>
2023-07-18 03:56:00 +0400

    fix/ profile page back button feature

--- src/components/ChangeUsername.vue
+++ src/components/ChangeUsername.vue
@@ -1,52 +1,35 @@
 <template>
   <Form @submit="submitHandler">
-    <section v-if="!showConfirmModal">
+    <section v-if="$route.name === 'changeName'">
       <section>
         <ProfileInput name="username" rules="required" label="enter_new_username" />
         <p class="text-red-500" v-if="errorMessage">{{ errorMessage }}</p>
       </section>
       <section class="flex justify-between pt-5">
-        <p @click="profileStore.showForm = true" class="py-2 px-1">{{ $t("cancel") }}</p>
+        <p @click="$router.push({ name: 'userProfile' })" class="py-2 px-1">
+          {{ $t("cancel") }}
+        </p>
         <button class="bg-red-600 rounded py-2 px-4">
           {{ $t("edit") }}
         </button>
       </section>
     </section>
-    <div v-if="showConfirmModal" class="grid grid-cols-1 divide-y divide-gray-500">
-      <section class="text-center py-6">
-        <p>{{ $t("sure_to_make_changes") }} ?</p>
-      </section>
-      <section class="pt-5 flex justify-between">
-        <p @click="showConfirmModal = false" class="py-3">{{ $t("cancel") }}</p>
-        <p @click="sendData()" class="bg-red-600 p-3 rounded">
-          {{ $t("confirm") }}
-        </p>
-      </section>
-    </div>
   </Form>
 </template>
 
 <script setup>
 import { Form } from "vee-validate";
-import { ref } from "vue";
 import ProfileInput from "@/components/ProfileInput.vue";
+import { useRouter } from "vue-router";
 import { useProfilePageStore } from "@/stores/profile/useProfilePageStore";
-import { userPassowrdUsernameUpdate } from "@/services/index";
+import { storeToRefs } from "pinia";
 
-const showConfirmModal = ref(false);
-const changedUsername = ref(null);
+const router = useRouter();
+const { checkErrorMessage } = useProfilePageStore();
+const { errorMessage } = storeToRefs(useProfilePageStore());
 
 function submitHandler(values) {
-  showConfirmModal.value = !showConfirmModal.value;
-  changedUsername.value = values.username;
+  router.push({ name: "youSure", query: { username: values.username } });
+  checkErrorMessage("");
 }
-const { toggleShowUsernameAlert } = useProfilePageStore();
-const profileStore = useProfilePageStore();
-
-const { sendData, errorMessage } = userPassowrdUsernameUpdate(
-  "username",
-  changedUsername,
-  toggleShowUsernameAlert,
-  showConfirmModal
-);
 </script>
